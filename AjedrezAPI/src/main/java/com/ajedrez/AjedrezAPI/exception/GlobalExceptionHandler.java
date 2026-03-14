622af590988113eab9f5c89ed622beaee81d9730
Alejandro Segovia <alejandross@cetys.edu.mx>
2026-03-13 20:20:43 -0700

    Versión final :) (espero xd)

--- AjedrezAPI/src/main/java/com/ajedrez/AjedrezAPI/exception/GlobalExceptionHandler.java
+++ AjedrezAPI/src/main/java/com/ajedrez/AjedrezAPI/exception/GlobalExceptionHandler.java
@@ -8,16 +8,19 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    //para cuando no se encuentra (nulo o vacio)
     @ExceptionHandler(NotFound.class)
     public ResponseEntity<String> handleNotFound(NotFound ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 
+    //cuando los datos enviados están mal
     @ExceptionHandler(Validacion.class)
     public ResponseEntity<String> handleValidacion(Validacion ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
+    //usado para cuando los datos no están mal, pero en el caso no lo permite enviar. Usado nomas en delete
     @ExceptionHandler(Conflicto.class)
     public ResponseEntity<String> handleConflicto(Conflicto ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
